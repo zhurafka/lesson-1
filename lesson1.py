471f8d42914767915a52fab9a50ec088aa9822f0
zhurafka <o-xana@yandex.ru>
2022-11-06 20:46:58 +0300

    create file lesson1

---
+++ lesson1.py
@@ -0,0 +1,22 @@
+print(2+3)
+print("Hello")
+print("Full name","Tom","Smith")
+
+user_id="12tomsmith438" #str
+print(user_id)
+
+user_id==234 #int
+print(user_id)
+
+user_id="12tomsmith438" #str
+print(type(user_id)) # str
+
+user_id=234 #int
+print(type(user_id)) # int
+
+first_number=2.0001
+second_number=5
+third_number=first_number/second_number
+print(third_number)
+
+print(round(third_number,4))
